c4b704639260463f9f047de3f042fee87de59bd7
Irina12626 <irina.gor@inbox.ru>
2022-09-19 19:39:09 +0300

    add ex04

---
+++ C#/Homework01/Ex04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите любое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int firstnumber = 1;
+
+while (firstnumber <= numberA)
+{
+    if (firstnumber % 2 == 0)
+        Console.Write(firstnumber + " ");
+    firstnumber++;
+}
