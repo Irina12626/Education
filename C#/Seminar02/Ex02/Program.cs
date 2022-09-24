eaeb1cf0a767240372b1a2d61028244a22791754
Irina12626 <irina.gor@inbox.ru>
2022-09-24 23:37:06 +0300

    add ex02

---
+++ C#/Seminar02/Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+// массив задается автоматически, искомое число оперделяется компьютером
+Console.Clear();
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0;
+Console.Write("Ищем = ");
+Console.WriteLine(find);
+Console.WriteLine("Массив: ");
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 22);
+    Console.Write(array[index] + "");
+    index += 1;
+}
+index = 0;
+while (index < count)
+{
+    if (array[index] = find)
+    {
+        Console.WriteLine("\nТакой элемент есть");
+    }
+    index += 1;
+}
+Console.WriteLine("\nEND");
