a7e88fbc57c5bddc475104b53c158af68b02350c
Irina12626 <irina.gor@inbox.ru>
2022-09-25 11:46:37 +0300

    Add all ex

---
+++ C#/Homework02/Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру, которая обозначает день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Введите корректное значение");
+    }
+    else Console.WriteLine("Этот день не является выходным");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
---
+++ C#/Homework02/Ex04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 228: Напишите программу, которая принимает на вход семь чисел и находит их среднее арифметическое
+
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+int length = array.Length;
+int index = 0;
+int sum = 0;
+while (index < length)
+{
+    sum = array[index] + sum;
+    index++;
+}
+int midarif = sum / length;
+Console.WriteLine($"Cреднее арифметическое число = {midarif}");
